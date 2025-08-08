67888e1f6da0bea4a70386cf0012567a8621df1b
marcosmedenou <mmedenou@vippinterstis.com>
2025-08-08 19:55:34 +0200

    delete files

--- api/settings/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-settings_module = os.getenv('DJANGO_SETTINGS_MODULE')
-
-if settings_module == 'api.settings.production':
-    from .production import *
-else:
-    from .development import *
