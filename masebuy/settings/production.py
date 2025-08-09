eeac9f0a5f5e87f462be7719fef4dcaf6e6e6401
marcos-medenou <marcosmedenou@gmail.com>
2025-08-10 00:12:37 +0200

    add postgres

--- masebuy/settings/production.py
+++ masebuy/settings/production.py
@@ -8,27 +8,27 @@ DEBUG = True
 ALLOWED_HOSTS = ['.vercel.app']
 
 # Database configuration remains the same
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('DATABASE_NAME'),
-#         'USER': config('DATABASE_USER'),
-#         'PASSWORD': config('DATABASE_PASSWORD'),
-#         'HOST': config('DATABASE_HOST'),
-#         'PORT': config('DATABASE_PORT'),
-#         'CONN_MAX_AGE': 600,
-#         'OPTIONS': {
-#             'sslmode': 'require',
-#             'client_encoding': 'UTF8',
-#         }
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',  # fichier local dans ton projet
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
+        'CONN_MAX_AGE': 600,
+        'OPTIONS': {
+            'sslmode': 'require',
+            'client_encoding': 'UTF8',
+        }
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',  # fichier local dans ton projet
+#     }
+# }
 
 
 # Security settings (keep these)
